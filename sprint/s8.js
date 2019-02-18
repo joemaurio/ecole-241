653db1fcfc59a751da628bd3c8d9c69b1e7a2f64
joe maurio <jmauriomaureno@gmail.com>
2019-02-18 18:01:23 +0100

    ajout de s9

---
+++ sprint/s8.js
@@ -0,0 +1,5 @@
+//Ecrire un programme qui demande un nombre et affiche sa table de multiplication jusqu'à 10.
+let nbre= prompt("saisir le nombre 4");
+		if(nbre==4){
+			for(let i=0;i<=10;i++){console.log(`${nbre}x ${i} = ${nbre*i}`)}
+		}else{console.log("juste avec 4")}
