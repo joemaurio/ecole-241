c67747cfeba114915af7015f5e06e0d1bd543105
joe maurio <jmauriomaureno@gmail.com>
2019-03-26 10:31:15 +0100

    Php okacode

--- sprint/s2.js
+++ sprint/s2.js
@@ -1,4 +1,4 @@
-//afficher 22 fois "Okacode" 22 fois
+//afficher 22 fois "Okacode"
 
 
 for(let affiche =1;affiche<=22;affiche++){console.log(affiche+": Okacode")}
--- sprint/s9.js
+++ sprint/s9.js
@@ -1,8 +1,9 @@
 //Ecrire un programme qui demande successivement 5 nombres à l’utilisateur, et qui lui dise ensuite quel était le plus grand parmi ces 5 nombres
-let nbre1 = prompt("Enter le premier nombre");
-let nbre2 = prompt("Enter le deuxieme nombre");
-let nbre3 = prompt("Enter le troisieme nombre");
-let nbre4 = prompt("Enter le quatrieme nombre");
-let nbre5 = prompt("Enter le cinquieme nombre");
+
+let nbre1 = prompt("Entrer le premier nombre");
+let nbre2 = prompt("Entrer le deuxieme nombre");
+let nbre3 = prompt("Entrer le troisieme nombre");
+let nbre4 = prompt("Entrer le quatrieme nombre");
+let nbre5 = prompt("Entrer le cinquieme nombre");
 let grand = Math.max(nbre1,nbre2,nbre3,nbre4,nbre5);
 console.log(`Le plus grand de ces nombres est :`+ grand);
