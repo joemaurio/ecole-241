fafd6442197f2150457b7d13ffcec9600fdf25b3
joe maurio <jmauriomaureno@gmail.com>
2019-03-26 15:59:24 +0100

    Php okacode act2

---
+++ sprint/php/act2.php
@@ -0,0 +1,17 @@
+<?php  
+$test = 'okacode';
+$nombre = 3;
+$a = "bonjour,";
+$b = "Comment vas?";
+$maurio = true;
+$f = "joe";
+$$f = 3;
+$table = array("joe","maurio","maureno");
+
+echo "je suis un <u>$test</u><br>";
+echo "j'aime le chiffre $nombre<br>"; 
+echo "$a $b";
+echo "<br>$maurio<br>";
+echo "$joe est plus grand que $maurio<br>";
+echo "Jmaurio ".$table["2"]." Damav";
+?>
